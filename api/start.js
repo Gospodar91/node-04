57bda9d0c841c882b49f8032a8d6e057700fe598
Andreiev <xsergeix@ukr.net>
2020-06-05 14:20:26 +0300

    fix

--- api/start.js
+++ api/start.js
@@ -4,3 +4,8 @@ require=require('esm')(module);
 require('dotenv').config({path:path.join(__dirname,'../env')})
 const {AuthServer}=require('./server')
 new AuthServer().start()
+
+
+// MONGODB_URL='mongodb+srv://Gospodar91:ZWBZq7inmJD9zJ7@cluster0-yfhnm.mongodb.net/auth?retryWrites=true&w=majority'
+// PORT=3000
+// BCRYPT_SALT=5
